011c8c18175197ae48363ca8b14b97eca0acb1b2
SarikaRAJPURE <sprajpure@gmail.com>
2022-12-23 22:06:50 -0500

    updated link for sound

--- index.js
+++ index.js
@@ -201,9 +201,9 @@ let shipsBattle = (ship1, ship2) => {
  */
 
 //4.set sound 
-const shootSound = new Audio("https://sarikarajpure.github.io/spacebattle/sounds/shoot.wav");
-const enemydeathSound = new Audio("https://sarikarajpure.github.io/spacebattle/sounds/enemy-death.wav");
-const backgroundsound= new Audio("https://sarikarajpure.github.io/spacebattle/sounds/backgroundmusic.mp3");
+const shootSound = new Audio("../sounds/shoot.wav");
+const enemydeathSound = new Audio("../sounds/enemy-death.wav");
+const backgroundsound= new Audio("../sounds/backgroundmusic.mp3");
 
 document.getElementById('yesbtn').addEventListener('click', () => {
     console.log("%c Spacebattle", "font-size: 40px");
