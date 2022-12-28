9e980bf73604a95b0c2967fc4d21b51488c39985
SarikaRAJPURE <sprajpure@gmail.com>
2022-12-28 10:25:59 -0500

    Changed page onload message

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ EnemyFleet.createalienShips();
 //console.log(EnemyFleet.alienShips);
 let fleet = EnemyFleet.alienShips;
 let loaddata = () => {
-    document.getElementById('msg').innerText=`This time aliens have send ${fleet.length} ships to attack Earth, start the game to attack them.`;
+    document.getElementById('msg').innerText=`Aliens have sent ${fleet.length} ships to attack Earth, start the game to attack them.`;
     //1. set data of my spaceship on web page
     let myshipname = document.getElementById('my-ship-name');
     myshipname.innerText = USSHelloWorld.name;
